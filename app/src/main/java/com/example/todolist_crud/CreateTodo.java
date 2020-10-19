7526443a4c8bf1134bc7ade784623bbc3f8686ae
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-10-19 02:33:29 +0100

    wip

--- app/src/main/java/com/example/todolist_crud/CreateTodo.java
+++ app/src/main/java/com/example/todolist_crud/CreateTodo.java
@@ -9,6 +9,6 @@ public class CreateTodo extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_todo);
+        setContentView(R.layout.activity_todo);
     }
 }
