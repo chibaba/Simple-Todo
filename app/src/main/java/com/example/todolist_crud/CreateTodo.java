84140e17e70dbb3c1f49679760c3f9f6b0e733f1
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-10-19 01:36:13 +0100

    wip

---
+++ app/src/main/java/com/example/todolist_crud/CreateTodo.java
@@ -0,0 +1,14 @@
+package com.example.todolist_crud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateTodo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_todo);
+    }
+}
